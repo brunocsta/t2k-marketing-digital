f68216b9d61d6d6212d6d8b3d2f744fdf0590dae
Bruno Costa <b.costamartins@gmail.com>
2024-07-19 10:23:21 -0300

    Make the autoplay behavior

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
 const carrossel = document.querySelector(".carrousel");
 const arrowBtns = document.querySelectorAll(".wrapper i");
 const firstCardWidth = carrossel.querySelector(".cards").offsetWidth;
 const carrosselChildrens = [...carrossel.children];
 let isArrastando = false,
 	posicaoX,
-	posicaoScrollEsquerdo;
+	posicaoScrollEsquerdo,
+	timeOutID;
 
 //Verifica o número de cartões que cabem na tela
 let cardPerView = Math.round(carrossel.offsetWidth / firstCardWidth);
+
+const autoplay = () => {
+	if (window.innerWidth < 800) return; //sem autoplay no mobile
+	//timer do autoplay
+	timeOutID = setTimeout(() => (carrossel.scrollLeft += firstCardWidth), 2500);
+}
+
+autoplay();
 
 //Insere os últimos cards no começo e os primeiros cards no fim da fila gerando um carrossel infinito
 carrosselChildrens
@@ -32,7 +42,7 @@ arrowBtns.forEach((btn) => {
 const arrastarFim = () => {
 	isArrastando = false;
 	carrossel.classList.remove("arrastando");
-};
+}
 
 const arrastarInicio = (e) => {
 	isArrastando = true;
@@ -40,13 +50,13 @@ const arrastarInicio = (e) => {
 	//posição inicial do cursos e do scroll
 	posicaoX = e.pageX;
 	posicaoScrollEsquerdo = carrossel.scrollLeft;
-};
+}
 
 const arrastar = (e) => {
 	if (!isArrastando) return; //sem movimento de drag retorna aqui a func.
 	//atualiza a posição do carrossel com base na posição do cursos e do scroll
 	carrossel.scrollLeft = posicaoScrollEsquerdo - (e.pageX - posicaoX); //
-};
+}
 
 const scrollInfinito = () => {
 	if (carrossel.scrollLeft === 0) {
@@ -61,9 +71,16 @@ const scrollInfinito = () => {
 		carrossel.scrollLeft = carrossel.offsetWidth;
 		carrossel.classList.remove("no-transition");
 	}
-};
+
+   //para o autoplay se o mouse estiver no carrossel
+   clearTimeout(timeOutID);
+   if(!wrapper.matches(":hover")) autoplay();
+}
 
 carrossel.addEventListener("mousedown", arrastarInicio);
 carrossel.addEventListener("mousemove", arrastar);
 document.addEventListener("mouseup", arrastarFim);
 carrossel.addEventListener("scroll", scrollInfinito);
+wrapper.addEventListener("mouseenter", () => {clearTimeout(timeOutID)});
+wrapper.addEventListener("mouseleave", () => {autoplay()});
+
