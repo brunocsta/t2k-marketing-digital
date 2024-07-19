4ef1ffdda4ca80bc33ca875ce7212f03e9fd9f0d
Bruno Costa <b.costamartins@gmail.com>
2024-07-19 10:04:24 -0300

    Make responsive, add behavior and set the display of caroussel component

---
+++ script.js
@@ -0,0 +1,69 @@
+const carrossel = document.querySelector(".carrousel");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCardWidth = carrossel.querySelector(".cards").offsetWidth;
+const carrosselChildrens = [...carrossel.children];
+let isArrastando = false,
+	posicaoX,
+	posicaoScrollEsquerdo;
+
+//Verifica o número de cartões que cabem na tela
+let cardPerView = Math.round(carrossel.offsetWidth / firstCardWidth);
+
+//Insere os últimos cards no começo e os primeiros cards no fim da fila gerando um carrossel infinito
+carrosselChildrens
+	.slice(-cardPerView)
+	.reverse()
+	.forEach((card) => {
+		carrossel.insertAdjacentHTML("afterbegin", card.outerHTML);
+	});
+
+carrosselChildrens.slice(0, cardPerView).forEach((card) => {
+	carrossel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+//EventListener para adicionar comportamento de scroll nas setas
+arrowBtns.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		carrossel.scrollLeft +=
+			btn.id === "left" ? -firstCardWidth : firstCardWidth;
+	});
+});
+
+const arrastarFim = () => {
+	isArrastando = false;
+	carrossel.classList.remove("arrastando");
+};
+
+const arrastarInicio = (e) => {
+	isArrastando = true;
+	carrossel.classList.add("arrastando");
+	//posição inicial do cursos e do scroll
+	posicaoX = e.pageX;
+	posicaoScrollEsquerdo = carrossel.scrollLeft;
+};
+
+const arrastar = (e) => {
+	if (!isArrastando) return; //sem movimento de drag retorna aqui a func.
+	//atualiza a posição do carrossel com base na posição do cursos e do scroll
+	carrossel.scrollLeft = posicaoScrollEsquerdo - (e.pageX - posicaoX); //
+};
+
+const scrollInfinito = () => {
+	if (carrossel.scrollLeft === 0) {
+		carrossel.classList.add("no-transition");
+		carrossel.scrollLeft = carrossel.scrollWidth - 2 * carrossel.offsetWidth;
+		carrossel.classList.remove("no-transition");
+	} else if (
+		Math.ceil(carrossel.scrollLeft) ===
+		carrossel.scrollWidth - carrossel.offsetWidth
+	) {
+		carrossel.classList.add("no-transition");
+		carrossel.scrollLeft = carrossel.offsetWidth;
+		carrossel.classList.remove("no-transition");
+	}
+};
+
+carrossel.addEventListener("mousedown", arrastarInicio);
+carrossel.addEventListener("mousemove", arrastar);
+document.addEventListener("mouseup", arrastarFim);
+carrossel.addEventListener("scroll", scrollInfinito);
